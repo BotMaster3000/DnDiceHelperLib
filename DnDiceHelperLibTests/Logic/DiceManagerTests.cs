18591679fed205899dcf36737d450b57198608ab
BotMaster3000 <jdgebauer@gmx.de>
2020-04-29 18:30:34 +0200

    Changed Git-Level to the scale of the whole Project, not just the individual Projects

---
+++ DnDiceHelperLibTests/Logic/DiceManagerTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DnDiceHelperLib.Logic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DnDiceHelperLib.Interfaces;
+using System.Linq;
+
+namespace DnDiceHelperLib.Logic.Tests
+{
+    [TestClass]
+    public class DiceManagerTests
+    {
+        private Random rand = new Random();
+
+        [TestMethod]
+        public void GetDiceTest_GetRandomDice()
+        {
+            for (int i = 0; i < 1000; ++i)
+            {
+                int sides = rand.Next();
+                IDice dice = DiceManager.GetDice(sides);
+                Assert.AreEqual(sides, dice.Sides);
+            }
+        }
+
+        [TestMethod]
+        public void GetDicesTest_GetMultipleDiceWithSameSide()
+        {
+            for (int i = 0; i < 100; ++i)
+            {
+                int diceToGenerate = rand.Next(100);
+                int sides = rand.Next();
+                IDice[] dices = DiceManager.GetDices(sides, diceToGenerate);
+                Assert.AreEqual(diceToGenerate, dices.Length);
+                foreach (IDice dice in dices)
+                {
+                    Assert.AreEqual(sides, dice.Sides);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void GetDicesTest_GetMultipleDiceWithMultipleSides()
+        {
+            IDictionary<int, int> sideCountAndTotalDice = new Dictionary<int, int>()
+            {
+                { 20, 51 },
+                { 21, 354 },
+                { 7638, 384 },
+                { 438, 534 },
+                { 43, 3547 },
+                { 7988, 67863 },
+                { 34, 843 },
+                { 7, 385 },
+            };
+            IDice[] dices = DiceManager.GetDices(sideCountAndTotalDice);
+            foreach (KeyValuePair<int, int> sidesAndDice in sideCountAndTotalDice)
+            {
+                int dicesWithSameSideCount = dices.Count(dice
+                    => dice.Sides == sidesAndDice.Key);
+                Assert.AreEqual(sidesAndDice.Value, dicesWithSameSideCount);
+            }
+        }
+    }
+}
---
+++ DnDiceHelperLibTests/Logic/DiceRollerTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DnDiceHelperLib.Logic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DnDiceHelperLib.Models;
+using DnDiceHelperLib.Interfaces;
+using System.Linq;
+
+namespace DnDiceHelperLib.Logic.Tests
+{
+    [TestClass]
+    public class DiceRollerTests
+    {
+        private readonly Random rand = new Random();
+
+        [TestMethod]
+        public void RollDiceTest()
+        {
+            DiceRoller roller = new DiceRoller();
+            for (int i = 0; i < 100; ++i)
+            {
+                List<IDice> dices = new List<IDice>();
+                for (int j = 0; j < rand.Next(1000); ++j)
+                {
+                    int totalDice = rand.Next(100);
+                    int sides = rand.Next(500000);
+                    dices.AddRange(DiceManager.GetDices(sides, totalDice));
+                }
+                int result = roller.RollDice(dices.ToArray());
+
+                int minDiceRoll = dices.Count;
+                Assert.IsTrue(result >= minDiceRoll);
+
+                int maxDiceRoll = dices.Sum(dice => dice.Sides);
+                Assert.IsTrue(result <= maxDiceRoll);
+            }
+        }
+    }
+}
