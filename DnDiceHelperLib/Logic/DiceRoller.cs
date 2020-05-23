627df1199bd472816020cdc9ad3be9255d7e464c
BotMaster3000 <jdgebauer@gmx.de>
2020-05-23 19:08:22 +0200

    Changed the RollDice(IDice[])-Method to sum the amount of sides on all dice and perform a single RandomManager.NextNumber to increase Performance

--- DnDiceHelperLib/Logic/DiceRoller.cs
+++ DnDiceHelperLib/Logic/DiceRoller.cs
@@ -2,6 +2,7 @@
 using DnDiceHelperLib.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DnDiceHelperLib.Logic
@@ -17,12 +18,9 @@ namespace DnDiceHelperLib.Logic
         public int RollDice(IDice[] dices)
         {
             ExceptionManager.ThrowArgumentNullExceptionIfParameterNull(nameof(dices), dices);
-            int result = 0;
-            for (int i = 0; i < dices.Length; ++i)
-            {
-                result += RandomManager.NextNumber(dices[i].Sides);
-            }
-            return result;
+            return RandomManager.NextNumber(dices
+                    .Sum(dice
+                        => dice.Sides));
         }
     }
 }
